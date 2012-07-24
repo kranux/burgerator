e2e5ba50fbf30fc8c2fe128f2142ca7f6d295fbc
kranux <andrius.kripaitis@gmail.com>
2012-07-24 21:34:21 +0300

    a

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,31 @@
-/**
- * Created with JetBrains WebStorm.
- * User: andrius
- * Date: 7/24/12
- * Time: 8:46 PM
- * To change this template use File | Settings | File Templates.
- */
-
 (function ($, window) {
     "use strict";
     $(function () {
+        var $portion1 = $("#portion-1"),
+            $portion2 = $("#portion-2");
 
+        $("input").on('change', function () {
+            var p1 = parseInt($portion1.val(), 10) || 1,
+                p2 = parseInt($portion2.val(), 10) || 1,
+                portions = p1 * p2;
+
+            $("input.amount-input").each(function () {
+                var $this = $(this),
+                    product = $this.data('product'),
+                    amount = parseFloat($this.val()) || 1,
+                    $totalAmountInput = $(".total-amount-input."+product),
+                    $priceInput = $("input.price-input."+product);
+
+                if (!$this.siblings('input[type="checkbox"]').is(':checked')) {
+                    $totalAmountInput.parents('li').hide();
+                    return;
+                }
+                var totalAmount = amount * portions,
+                    multiplier = parseFloat($priceInput.data('multiplier')) || 1,
+                   productPrice = totalAmount * parseFloat($priceInput.val()) * multiplier;
+                $totalAmountInput.text(totalAmount).parents('li').show();
+                $totalAmountInput.siblings('.total-price-input').text(productPrice);
+            });
+        });
     });
 })(jQuery, window);
