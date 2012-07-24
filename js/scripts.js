5ff45d521d716dea3195f85f06460386b6d163aa
kranux <andrius.kripaitis@gmail.com>
2012-07-24 21:20:39 +0300

    Some mistypes fixed, some markup edited.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: andrius
+ * Date: 7/24/12
+ * Time: 8:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+(function ($, window) {
+    "use strict";
+    $(function () {
+
+    });
+})(jQuery, window);
